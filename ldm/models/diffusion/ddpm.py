34591ab29c5bc6bb8287a0b706f0abadd4e2c732
Paweł Skierś <pawelsk269@gmail.com>
2024-11-05 19:01:21 +0100

    Make the code faster

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -354,14 +354,14 @@ class DDPM(pl.LightningModule):
 
         return loss
 
-    @torch.no_grad()
-    def validation_step(self, batch, batch_idx):
-        _, loss_dict_no_ema = self.shared_step(batch)
-        with self.ema_scope():
-            _, loss_dict_ema = self.shared_step(batch)
-            loss_dict_ema = {key + '_ema': loss_dict_ema[key] for key in loss_dict_ema}
-        self.log_dict(loss_dict_no_ema, prog_bar=False, logger=True, on_step=False, on_epoch=True)
-        self.log_dict(loss_dict_ema, prog_bar=False, logger=True, on_step=False, on_epoch=True)
+    # @torch.no_grad()
+    # def validation_step(self, batch, batch_idx):
+    #     _, loss_dict_no_ema = self.shared_step(batch)
+    #     with self.ema_scope():
+    #         _, loss_dict_ema = self.shared_step(batch)
+    #         loss_dict_ema = {key + '_ema': loss_dict_ema[key] for key in loss_dict_ema}
+    #     self.log_dict(loss_dict_no_ema, prog_bar=False, logger=True, on_step=False, on_epoch=True)
+    #     self.log_dict(loss_dict_ema, prog_bar=False, logger=True, on_step=False, on_epoch=True)
 
     def on_train_batch_end(self, *args, **kwargs):
         if self.use_ema:
